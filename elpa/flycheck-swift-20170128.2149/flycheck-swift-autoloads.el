88ae7cb7d819ccef54652461034f69c8b67ac9e2
liangkai <kevin.scnu@gmail.com>
2018-02-03 19:54:34 +0800

    add ibuffer packages

---
+++ elpa/flycheck-swift-20170128.2149/flycheck-swift-autoloads.el
@@ -0,0 +1,22 @@
+;;; flycheck-swift-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "flycheck-swift" "flycheck-swift.el" (23157
+;;;;;;  39791 524423 758000))
+;;; Generated autoloads from flycheck-swift.el
+
+(autoload 'flycheck-swift-setup "flycheck-swift" "\
+Setup Flycheck for Swift.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-swift-autoloads.el ends here
---
+++ elpa/flycheck-swift-20170128.2149/flycheck-swift-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-swift" "20170128.2149" "Flycheck extension for Apple's Swift." '((emacs "24.4") (flycheck "0.25")) :commit "4c5ad401252400a78da395fd56a71e67ff8c2761" :keywords '("languages" "swift"))
