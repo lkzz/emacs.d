3855659b9e60cf51931dea6827890989162d977c
liangkai <kevin.scnu@gmail.com>
2018-02-12 11:46:39 +0800

    remove packages in elpa

--- elpa/go-impl-20170125.752/go-impl-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; go-impl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "go-impl" "go-impl.el" (23169 3234 334204 437000))
-;;; Generated autoloads from go-impl.el
-
-(autoload 'go-impl "go-impl" "\
-
-
-\(fn RECEIVER INTERFACE)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-impl-autoloads.el ends here
