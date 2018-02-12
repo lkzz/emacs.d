166e7e3a2c4af6d21d68d49980a92f3a32a9ebdd
liangkai <kevin.scnu@gmail.com>
2018-02-12 11:46:39 +0800

    remove packages in elpa

--- elpa/golint-20150414.1730/golint-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "golint" "20150414.1730" "lint for the Go source code" 'nil :commit "e14d9b0f1d332b1420c1ffa32562ad2dc84d645d" :url "https://github.com/golang/lint")
