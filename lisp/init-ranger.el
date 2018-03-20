bbe52dc7b92d03deeb5f79508286f5d5e99f2426
liangkai <kevin.scnu@gmail.com>
2018-03-21 01:03:52 +0800

    set ranger-show-hidden t

--- lisp/init-ranger.el
+++ lisp/init-ranger.el
@@ -15,8 +15,13 @@
   (setq ranger-excluded-extensions '("mkv" "iso" "mp4" "pdf"))
   ;; Binary files will not be previewed, if this variable is set to t
   (setq ranger-dont-show-binary t)
+  (setq ranger-preview-file t)
   ;; Files that are larger than the max file size (in MB) variable, won't be previewed.
   (setq ranger-max-preview-size 20)
+  ;; Hide dotfiles
+  (setq ranger-show-hidden nil)
+  (setq ranger-parent-depth 0)
+  (setq ranger-max-parent-width 0.12)
   ;; Show cursor in ranger
   (setq ranger-hide-cursor nil))
 
