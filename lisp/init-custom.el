ad4ca747b09bd222129e530ee3c70276b1469b4b
liangkai <kevin.scnu@gmail.com>
2018-04-16 00:26:51 +0800

    add cleanup-buffer function

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -109,6 +109,22 @@
   (switch-to-buffer (get-buffer-create "*scratch*"))
   (lisp-interaction-mode))
 
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a 'before-save-hook, and that
+might be bad."
+  (interactive)
+  (untabify (point-min) (point-max))
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (cleanup-buffer-safe)
+  (indent-buffer))
+
 (defun kevin/make-frame ()
   "New a frame,and erase buffer."
   (interactive)
