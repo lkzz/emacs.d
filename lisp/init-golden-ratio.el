fc3ce61cc075517089fcddfff9eedb04deabf9ad
liangkai <kevin.scnu@gmail.com>
2018-04-15 23:48:04 +0800

    golden ratio init

---
+++ lisp/init-golden-ratio.el
@@ -0,0 +1,11 @@
+;;; init-golden-ratio.el --- golden ratio config.
+;;; Commentary:
+;;; Code:
+
+(use-package golden-ratio
+  :ensure t
+  :defer t
+  :commands (golden-ratio-mode))
+
+(provide 'init-golden-ratio)
+;;; init-golden-ratio.el ends here
