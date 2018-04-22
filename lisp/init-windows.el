845ca84179d8abd2bec84cf691af186c05883eeb
liangkai <kevin.scnu@gmail.com>
2018-04-22 16:30:33 +0800

    more personal

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -50,83 +50,77 @@
   :bind ("C-x C-z" . zoom-window-zoom)
   :init (setq zoom-window-mode-line-color "DarkGreen"))
 
-;; ;; Popup Window Manager
-;; (use-package popwin
-;;   :ensure t
-;;   :defer t
-;;   :commands popwin-mode
-;;   :init (add-hook 'after-init-hook #'popwin-mode)
-;;   :config
-;;   (bind-key "C-z" popwin:keymap)
+(use-package centered-window
+  :ensure t
+  :init (setq cwm-use-vertical-padding t
+              cwm-frame-internal-border 15
+              cwm-incremental-padding t
+              cwm-left-fringe-ratio 0 ))
 
-;;   ;; don't use default value but manage it ourselves
-;;   (setq popwin:special-display-config
-;;         '(;; Emacs
-;;           ("*Help*" :dedicated t :position bottom :stick nil :noselect nil)
-;;           ("*compilation*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
-;;           ("*Compile-Log*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
-;;           ("*Warnings*" :dedicated t :position bottom :stick t :noselect t)
-;;           ("*Completions*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*Shell Command Output*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("\*Async Shell Command\*.+" :regexp t :position bottom :stick t :noselect nil)
-;;           ("^*Man.+*$" :regexp t :position bottom :stick nil :noselect nil :height 0.4)
-;;           ("^*WoMan.+*$" :regexp t :position bottom)
-;;           ("^*Backtrace.+*$" :regexp t :dedicated t :position bottom :stick t :noselect nil)
-
-;;           ;; Kill Ring
-;;           ("*Kill Ring*" :dedicated t :position bottom)
-
-;;           ;; Flycheck
-;;           ("\*flycheck errors\*.+*$" :regexp t :position bottom :stick t :noselect nil)
-
-;;           ;; Youdao dict
-;;           ("*Youdao Dictionary*" :dedicated t :position bottom)
-
-;;           ;; Paradox
-;;           ("*Paradox Report*" :dedicated t :position bottom :noselect nil)
-
-;;           ;; List
-;;           ("*Colors*" :dedicated t :position bottom)
-;;           ("*Process List*" :dedicated t :position bottom)
-;;           ("*Process-Environment*" :dedicated t :position bottom)
-
-;;           ;; undo-tree
-;;           (" *undo-tree*" :dedicated t :position right :stick t :noselect nil :width 60)
-
-;;           ;; Search
-;;           ("*grep*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*ag search*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
-;;           ("*rg*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
-;;           ("*pt-search*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
-;;           ("*Occur*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("\*ivy-occur.+*$" :regexp t :position bottom :stick t :noselect nil)
-;;           ("*xref*" :dedicated t :position bottom :stick nil :noselect nil)
-
-;;           ;; VC
-;;           ("*vc-diff*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*vc-change-log*" :dedicated t :position bottom :stick t :noselect nil)
-
-;;           ;; Magit
-;;           ;; (magit-status-mode :dedicated t :position bottom :stick t :height 0.5)
-;;           ;; (magit-diff-mode :dedicated t :position bottom :stick t :noselect t :height 0.5)
-
-;;           ;; Script
-;;           ("*shell*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*Python*" :dedicated t :position bottom :stick t :noselect t)
-;;           ("*Ruby*" :dedicated t :position bottom :stick t :noselect t)
-;;           ("*quickrun*" :dedicated t :position bottom :stick t :noselect t)
-
-;;           ;; Go
-;;           ("^*godoc.+*$" :regexp t :position bottom :stick nil :noselect nil)
-;;           ("*golint*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*govet*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*go-guru-output*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*Gofmt Errors*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*Go Test*" :dedicated t :position bottom :stick t :noselect nil)
-
-;;           ;; Test
-;;           ("*ert*" :dedicated t :position bottom :stick t :noselect nil)
-;;           ("*nosetests*" :dedicated t :position bottom :stick t :noselect nil))))
+;; Popup Window Manager
+(use-package popwin
+  :ensure t
+  :defer t
+  :commands popwin-mode
+  :init (add-hook 'after-init-hook #'popwin-mode)
+  :config
+  (bind-key "C-z" popwin:keymap)
+  ;; don't use default value but manage it ourselves
+  (setq popwin:special-display-config
+        '(;; Emacs
+          ("*Help*" :dedicated t :position bottom :stick nil :noselect nil)
+          ("*compilation*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
+          ("*Compile-Log*" :dedicated t :position bottom :stick t :noselect t :height 0.4)
+          ("*Warnings*" :dedicated t :position bottom :stick t :noselect t)
+          ("*Completions*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*Shell Command Output*" :dedicated t :position bottom :stick t :noselect nil)
+          ("\*Async Shell Command\*.+" :regexp t :position bottom :stick t :noselect nil)
+          ("^*Man.+*$" :regexp t :position bottom :stick nil :noselect nil :height 0.4)
+          ("^*WoMan.+*$" :regexp t :position bottom)
+          ("^*Backtrace.+*$" :regexp t :dedicated t :position bottom :stick t :noselect nil)
+          ;; Kill Ring
+          ("*Kill Ring*" :dedicated t :position bottom)
+          ;; Flycheck
+          ("\*flycheck errors\*.+*$" :regexp t :position bottom :stick t :noselect nil)
+          ;; Youdao dict
+          ("*Youdao Dictionary*" :dedicated t :position bottom)
+          ;; Paradox
+          ("*Paradox Report*" :dedicated t :position bottom :noselect nil)
+          ;; List
+          ("*Colors*" :dedicated t :position bottom)
+          ("*Process List*" :dedicated t :position bottom)
+          ("*Process-Environment*" :dedicated t :position bottom)
+          ;; undo-tree
+          (" *undo-tree*" :dedicated t :position right :stick t :noselect nil :width 60)
+          ;; Search
+          ("*grep*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*ag search*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
+          ("*rg*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
+          ("*pt-search*" :dedicated t :position bottom :stick t :noselect nil :height 0.4)
+          ("*Occur*" :dedicated t :position bottom :stick t :noselect nil)
+          ("\*ivy-occur.+*$" :regexp t :position bottom :stick t :noselect nil)
+          ("*xref*" :dedicated t :position bottom :stick nil :noselect nil)
+          ;; VC
+          ("*vc-diff*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*vc-change-log*" :dedicated t :position bottom :stick t :noselect nil)
+          ;; Magit
+          (magit-status-mode :dedicated t :position bottom :stick t :height 0.5)
+          (magit-diff-mode :dedicated t :position bottom :stick t :noselect t :height 0.5)
+          ;; Script
+          ("*shell*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*Python*" :dedicated t :position bottom :stick t :noselect t)
+          ("*Ruby*" :dedicated t :position bottom :stick t :noselect t)
+          ("*quickrun*" :dedicated t :position bottom :stick t :noselect t)
+          ;; Go
+          ("^*godoc.+*$" :regexp t :position bottom :stick nil :noselect nil)
+          ("*golint*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*govet*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*go-guru-output*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*Gofmt Errors*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*Go Test*" :dedicated t :position bottom :stick t :noselect nil)
+          ;; Test
+          ("*ert*" :dedicated t :position bottom :stick t :noselect nil)
+          ("*nosetests*" :dedicated t :position bottom :stick t :noselect nil))))
 
 ;; Easy window config switching
 (use-package eyebrowse
