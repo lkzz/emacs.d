8e057f301fd4e8b81eaf8f4b2930afe863f499fe
kevin leung <kevin.scnu@gmail.com>
2018-06-11 10:23:51 +0800

    much more customize

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -21,15 +21,24 @@
 (defvar kevin/cache-directory (expand-file-name "cache/" user-emacs-directory)
   "Emacs cache directory.")
 
+(defvar kevin/default-directory "~/Code/gopath/src/go-common/business"
+  "Default project directory.")
+
+(defvar kevin/user-name "kevin leung"
+  "Default username.")
+
+(defvar kevin/mail-address "kevin.scnu@gmail.com"
+  "Default email address.")
+
 (defcustom my-theme 'default
   "Set color theme."
   :type '(choice
-          (const :tag "Monokai theme" default)
+          (const :tag "zenburn theme" default)
           (const :tag "Tomorrow night theme" dark)
           (const :tag "Leuven light theme" light)
           (const :tag "Doom theme" doom)))
 
-(setq my-theme 'dark)
+(setq my-theme 'default)
 
 (provide 'init-custom)
 ;;; init-custom.el ends here
