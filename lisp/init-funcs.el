aaeb1b07a0bc8bc37c5ddae39ecbdeb7d678b384
liangkai <kevin.scnu@gmail.com>
2018-06-29 23:49:46 +0800

    refactor buffer

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -149,63 +149,6 @@ minor-mode, the third argument should be non nil."
   (do-applescript
    "do shell script \"open -a Google Chrome\"\n"))
 
-(defun kevin/revert-buffer-no-confirm ()
-  "Revert buffer without confirm."
-  (interactive)
-  (revert-buffer t t))
-
-(defun indent-buffer()
-  "Indent buffer."
-  (interactive)
-  (indent-region (point-min) (point-max)))
-
-(defun kevin/indent-region-or-buffer()
-  "Indent regex or buffer."
-  (interactive)
-  (save-excursion
-    (if (region-active-p)
-        (progn
-          (indent-region (region-beginning) (region-end))
-          (message "Indent selected region."))
-      (progn
-        (indent-buffer)
-        (message "Indent buffer.")))))
-
-;; Kill all buffers except scratch buffer
-(defun kevin/kill-all-buffers ()
-  "Kill all buffers."
-  (interactive)
-  (mapc (lambda (x) (kill-buffer x)) (buffer-list))
-  (delete-other-windows))
-
-;; Kill all buffers except the current one.
-(defun kevin/kill-other-buffers ()
-  "Kill all other buffers."
-  (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
-
-(defun kevin/create-scratch-buffer nil
-  "Create a scratch buffer."
-  (interactive)
-  (switch-to-buffer (get-buffer-create "*scratch*"))
-  (lisp-interaction-mode))
-
-(defun cleanup-buffer-safe ()
-  "Perform a bunch of safe operations on the whitespace content of a buffer.
-Does not indent buffer, because it is used for a 'before-save-hook, and that
-might be bad."
-  (interactive)
-  (untabify (point-min) (point-max))
-  (delete-trailing-whitespace)
-  (set-buffer-file-coding-system 'utf-8))
-
-(defun cleanup-buffer ()
-  "Perform a bunch of operations on the whitespace content of a buffer.
-Including indent-buffer, which should not be called automatically on save."
-  (interactive)
-  (cleanup-buffer-safe)
-  (indent-buffer))
-
 (defun kevin/goto-match-parent ()
   "Go to the matching  if on (){}[], similar to vi style of %."
   (interactive)
@@ -221,15 +164,6 @@ Including indent-buffer, which should not be called automatically on save."
   "Check if buffer size is larger than 1M or has more than 5000 lines."
   (or (> (buffer-size) (* 1024 1024))
       (> (line-number-at-pos (point-max)) 5000)))
-
-
-(defun kevin/remove-elc-on-save ()
-  "If you're saving an elisp file, likely the .elc is no longer valid."
-  (make-local-variable 'after-save-hook)
-  (add-hook 'after-save-hook
-            (lambda ()
-              (if (file-exists-p (concat buffer-file-name "c"))
-                  (delete-file (concat buffer-file-name "c"))))))
 
 (defun kevin/make-frame ()
   "New a frame,and erase buffer."
@@ -279,28 +213,6 @@ Including indent-buffer, which should not be called automatically on save."
         (message x)
         (append my-list x)
         ))
-
-(defun kevin/normal-buffer ()
-  (or (not buffer-read-only)
-      (buffer-file-name)))
-
-(defun kevin/switch-to-next-buffer ()
-  (interactive)
-  (unless (minibufferp)
-    (let ((p t) (bn (buffer-name)))
-      (switch-to-next-buffer)
-      (while (and p (not (kevin/normal-buffer)))
-	    (switch-to-next-buffer)
-	    (when (string= bn (buffer-name)) (setq p nil))))))
-
-(defun kevin/switch-to-prev-buffer ()
-  (interactive)
-  (unless (minibufferp)
-    (let ((p t) (bn (buffer-name)))
-      (switch-to-prev-buffer)
-      (while (and p (not (kevin/normal-buffer)))
-	    (switch-to-prev-buffer)
-	    (when (string= bn (buffer-name)) (setq p nil))))))
 
 (defun kevin/open-init-file ()
   "Open emacs init file."
