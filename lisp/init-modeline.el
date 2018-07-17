037fcacc307af7de2317e756715cac2e368593a4
kevin leung <kevin.scnu@gmail.com>
2018-07-17 20:34:57 +0800

    refactor modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,13 +1,20 @@
-;;; init-modeline.el --- modeline config for emacs.
+;;; init-modeline.el. -*- lexical-binding: t; -*-
+;;
+;; Author: kevin <kevin.scnu@gmail.com>
+;; URL: https://github.com/lkzz/emacs.d
+;;
 ;;; Commentary:
+;;             modeline config.
 ;;; Code:
 
+;;;###autoload
 (defmacro modeline-define-segment (name body)
   "Create function NAME by wrapping BODY."
   `(defun ,name
        (&optional face)
      (modeline-render-str ,body face)))
 
+;;;###autoload
 (defun modeline-render-segment (item)
   "Render a modeline segment ITEM."
   (cond
@@ -16,18 +23,21 @@
                 'face (plist-get (cdr item) :face)))
    (t item)))
 
+;;;###autoload
 (defun modeline-render-segment-list (items)
   "Render a list of powerline VALUES."
   (mapconcat 'modeline-render-segment items ""))
 
+;;;###autoload
 (defun modeline-render-str (str &optional face)
-  "Render STR as mode-line data using FACE."
+  "Render STR as mode-line data using FACE and left space on the right side."
   (when str
     (let ((fstr (format-mode-line str)))
       (if face
-          (propertize fstr 'face face)
-        fstr))))
+          (concat (propertize fstr 'face face) " ")
+        (concat fstr " ")))))
 
+;;;###autoload
 (defun modeline-width (items)
   "Get the length of ITEMS."
   (if items
@@ -40,6 +50,7 @@
            (modeline-width (cdr items))))
     0))
 
+;;;###autoload
 (defun modeline-fill (reserve &optional face)
   "Return empty space using FACE and leaving RESERVE space on the right."
   (unless reserve
@@ -48,81 +59,25 @@
               'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
               'face (if face face 'mode-line)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Custom faces;;;;;;;;;;;;;;;;;;;;;;;
-(defface doom-modeline-buffer-path
-  '((t (:inherit mode-line-emphasis :bold t)))
-  "Face used for the dirname part of the buffer path.")
-
-(defface doom-modeline-buffer-file
-  '((t (:inherit mode-line-buffer-id)))
-  "Face used for the filename part of the mode-line buffer path.")
-
-(defface doom-modeline-buffer-modified
-  '((t (:inherit error :background nil :bold t)))
-  "Face used for the 'unsaved' symbol in the mode-line.")
-
-(defface doom-modeline-buffer-major-mode
-  '((t (:inherit mode-line-emphasis :bold t)))
-  "Face used for the major-mode segment in the mode-line.")
-
-(defface doom-modeline-highlight
-  '((t (:inherit mode-line-emphasis)))
-  "Face for bright segments of the mode-line.")
-
-(defface doom-modeline-panel
-  '((t (:inherit mode-line-highlight)))
-  "Face for 'X out of Y' segments, such as `doom-modeline--anzu', `doom-modeline--evil-substitute' and
-`iedit'")(eval-and-compile
-  (defvar doom-modeline-fn-alist ())
-  (defvar doom-modeline-var-alist ()))
-
-(defface doom-modeline-info
-  `((t (:inherit success :bold t)))
-  "Face for info-level messages in the modeline. Used by `*vc'.")
-
-(defface doom-modeline-warning
-  `((t (:inherit warning :bold t)))
-  "Face for warnings in the modeline. Used by `*flycheck'")
-
-(defface doom-modeline-urgent
-  `((t (:inherit error :bold t)))
-  "Face for errors in the modeline. Used by `*flycheck'")
-
-;; Bar
-(defface doom-modeline-bar '((t (:inherit highlight)))
-  "The face used for the left-most bar on the mode-line of an active window.")
-
-(defface doom-modeline-eldoc-bar '((t (:inherit shadow)))
-  "The face used for the left-most bar on the mode-line when eldoc-eval is
-active.")
-
-(defface doom-modeline-inactive-bar '((t (:inherit warning :inverse-video t)))
-  "The face used for the left-most bar on the mode-line of an inactive window.")
-
-(defface doom-modeline-persp '((t ()))
-  "The face used for persp number.")
-
-(defface doom-modeline-eyebrowse '((t ()))
-  "The face used for eyebrowse.")
-
-(defface doom-modeline-bracket '((t (:inherit shadow)))
-  "The face used for brackets around the project.")
-
+;;;###autoload
 (defun kevin/maybe-octicon-icon (&rest args)
   "Display octicon via `ARGS'."
   (when (display-graphic-p)
     (apply 'all-the-icons-octicon args)))
 
+;;;###autoload
 (defun kevin/maybe-faicon-icon (&rest args)
   "Display font awesome icon via `ARGS'."
   (when (display-graphic-p)
     (apply 'all-the-icons-faicon args)))
 
+;;;###autoload
 (defun kevin/maybe-material-icon (&rest args)
   "Display material icon via `ARGS'."
   (when (display-graphic-p)
     (apply 'all-the-icons-material args)))
 
+;;;###autoload
 (defun kevin/material-icon-with-text (icon &optional text face voffset)
   "Displays an material ICON with FACE, followed by TEXT."
   (concat
@@ -132,6 +87,7 @@ active.")
       " "
       (if text (propertize text 'face face))))))
 
+;;;###autoload
 (defun kevin/faicon-icon-with-text (icon &optional text face voffset)
   "Displays an faicon ICON with FACE, followed by TEXT."
   (concat
@@ -141,8 +97,7 @@ active.")
       " "
       (if text (propertize text 'face face))))))
 
-
-
+;;;###autoload
 (defun shorten-directory (dir max-length)
   "Setup a directory(`DIR') `MAX-LENGTH' characters."
   (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
@@ -156,43 +111,43 @@ active.")
       (setq output (concat ".../" output)))
     output))
 
-(modeline-define-segment buffer-info
+;;;###autoload
+(defun modeline-active ()
+  "Return whether the current window is active."
+  (eq (frame-selected-window) (selected-window)))
+
+(modeline-define-segment buffer-info-segment
                          (concat (cond (buffer-read-only
-                                        (concat (kevin/maybe-material-icon
-                                                 "lock"
-                                                 :face 'mode-line-buffer-id
-                                                 :v-adjust -0.05)
-                                                " "))
+                                        (kevin/maybe-material-icon "lock"
+                                                                   :face 'warning
+                                                                   :v-adjust -0.05))
                                        ((buffer-modified-p)
-                                        (concat (kevin/maybe-faicon-icon
-                                                 "floppy-o"
-                                                 :face 'doom-modeline-buffer-modified
-                                                 :v-adjust -0.0575)
-                                                " "))
+                                        (kevin/maybe-faicon-icon "floppy-o"
+                                                                 :face 'error
+                                                                 :v-adjust -0.0575))
                                        ((and buffer-file-name
                                              (not (file-exists-p buffer-file-name)))
-                                        (concat (kevin/maybe-octicon-icon
-                                                 "circle-slash"
-                                                 :face 'doom-modeline-urgent
-                                                 :v-adjust -0.05)
-                                                " "))
+                                        (kevin/maybe-octicon-icon "circle-slash"
+                                                                  :face 'error
+                                                                  :v-adjust -0.05))
                                        ((buffer-narrowed-p)
-                                        (concat (kevin/maybe-octicon-icon
-                                                 "fold"
-                                                 :face 'doom-modeline-warning
-                                                 :v-adjust -0.05)
-                                                " ")))
+                                        (kevin/maybe-octicon-icon "fold"
+                                                                  :face 'warning
+                                                                  :v-adjust -0.05))
+                                       )
                                  (if (buffer-file-name)
-                                     (concat (shorten-directory default-directory 15) (file-relative-name buffer-file-name) " ")
-                                   "%b ")))
+                                     (concat (shorten-directory default-directory 15) (file-relative-name buffer-file-name))
+                                   "%b")))
 
-(modeline-define-segment timestamp-info
-                         (format-time-string "%H:%M "))
 
-(modeline-define-segment position-info
-                         (format-mode-line "%l:%c "))
 
-(modeline-define-segment buffer-encoding
+(modeline-define-segment timestamp-info-segment
+                         (format-time-string "%H:%M"))
+
+(modeline-define-segment position-info-segment
+                         (format-mode-line "%l:%c"))
+
+(modeline-define-segment buffer-encoding-segment
                          (concat (pcase (coding-system-eol-type buffer-file-coding-system)
                                    (0 "LF ")
                                    (1 "CRLF ")
@@ -200,63 +155,112 @@ active.")
                                  (let ((sys (coding-system-plist buffer-file-coding-system)))
                                    (cond ((memq (plist-get sys :category) '(coding-category-undecided coding-category-utf-8))
                                           "UTF-8")
-                                         (t (upcase (symbol-name (plist-get sys :name))))))
-                                 " "))
+                                         (t (upcase (symbol-name (plist-get sys :name))))))))
 
-(modeline-define-segment vc-info
+(modeline-define-segment vsc-info-segment
                          (when (bound-and-true-p vc-mode)
                            (cond ((string-match "Git[:-]" vc-mode)
                                   (let ((branch (mapconcat 'concat (cdr (split-string vc-mode "[:-]")) "-")))
-                                    (kevin/faicon-icon-with-text
-                                     "code-fork"
-                                     (format "%s " branch)
-                                     'doom-modeline-urgent
-                                     -0.05)
-                                    ))
-                                 (t (format "%s " vc-mode)))))
+                                    (kevin/faicon-icon-with-text "code-fork"
+                                                                 branch
+                                                                 'success
+                                                                 0.05) ))
+                                 (t vc-mode))))
 
-(modeline-define-segment flycheck-info
+(modeline-define-segment flycheck-segment
                          (when (bound-and-true-p flycheck-mode)
-                           (concat
-                            (pcase flycheck-last-status-change
-                              (`finished (if flycheck-current-errors
-                                             (let-alist (flycheck-count-errors flycheck-current-errors)
-                                               (let ((sum (+ (or .error 0) (or .warning 0))))
-                                                 (kevin/material-icon-with-text "do_not_disturb_alt"
-                                                                                (number-to-string sum)
-                                                                                (if .error 'doom-modeline-urgent 'doom-modeline-warning)
-                                                                                -0.25)))
-                                           (kevin/material-icon-with-text "check" " " 'doom-modeline-info)))
-                              (`running     (kevin/material-icon-with-text "access_time" " " 'font-lock-doc-face -0.25))
-                              (`no-checker  (kevin/material-icon-with-text "sim_card_alert" " -" 'font-lock-doc-face))
-                              (`errored     (kevin/material-icon-with-text "sim_card_alert" " Error" 'doom-modeline-urgent))
-                              (`interrupted (kevin/material-icon-with-text "pause" " Interrupted" 'font-lock-doc-face)))
-                            " ")))
+                           (pcase flycheck-last-status-change
+                             (`finished (if flycheck-current-errors
+                                            (let-alist (flycheck-count-errors flycheck-current-errors)
+                                              (let ((sum (+ (or .error 0) (or .warning 0))))
+                                                (kevin/material-icon-with-text "do_not_disturb_alt"
+                                                                               (number-to-string sum)
+                                                                               (if .error 'error 'warning)
+                                                                               -0.15)))
+                                          (kevin/material-icon-with-text "check" nil 'mode-line)))
+                             (`running     (kevin/material-icon-with-text "access_time" nil 'font-lock-doc-face -0.25))
+                             (`no-checker  (kevin/material-icon-with-text "sim_card_alert" "-" 'font-lock-doc-face))
+                             (`errored     (kevin/material-icon-with-text "sim_card_alert" "Error" 'error))
+                             (`interrupted (kevin/material-icon-with-text "pause" "Interrupted" 'font-lock-doc-face)))
+                           ))
 
-(modeline-define-segment minor-modes
-                         (format-mode-line minor-mode-alist))
+;; Return simplifyed major mode name.
+(modeline-define-segment major-mode-segment
+                         (let* ((major-name (format-mode-line "%m"))
+                                (replace-table '(Emacs-Lisp "Elisp"
+                                                            Python "Py"
+                                                            Shell ">"
+                                                            Makrdown "MD"
+                                                            GFM "𝓜"
+                                                            Org "lrg"
+                                                            Text "𝓣ext"
+                                                            Fundamental "ℱ"
+                                                            ))
+                                (replace-name (plist-get replace-table (intern major-name))))
+                           (if replace-name replace-name major-name)))
 
-(setq-default mode-line-format
-              '("%e"
-                (:eval
-                 (let* ((lhs (list (buffer-info)
-                                   (vc-info)
-                                   (minor-modes 'mode-line)
-                                   " "
-                                   (position-info 'doom-modeline-warning)
-                                   ))
-                        (rhs (list
-                              (flycheck-info)
-                              ;; (buffer-encoding)
-                              (timestamp-info 'mode-line)
-                              ))
-                        )
-                   (concat
-                    (modeline-render-segment-list lhs)
-                    (modeline-fill (modeline-width rhs))
-                    (modeline-render-segment-list rhs)
-                    )))))
+(modeline-define-segment minor-mode-segment
+                         (when (> (window-width) 120)
+                           (format-mode-line minor-mode-alist)))
 
+(modeline-define-segment window-number-segment
+                         (when (bound-and-true-p window-numbering-mode)
+                           (let* ((num (window-numbering-get-number))
+                                  (str (when num (int-to-string num))))
+                             (cond
+                              ((string= "1" str) "➊")
+                              ((string= "2" str) "➋")
+                              ((string= "3" str) "➌")
+                              ((string= "4" str) "➍")
+                              ((string= "5" str) "➎")
+                              ((string= "6" str) "➏")
+                              ((string= "7" str) "➐")
+                              ((string= "8" str) "➑")
+                              ((string= "9" str) "➒")
+                              ((string= "0" str) "➓"))
+                             )))
+
+
+(modeline-define-segment evil-tag-segment
+                         (when (bound-and-true-p evil-mode)
+                           (setq evil-normal-state-tag (kevin/faicon-icon-with-text "chevron-right" "NO" 'mode-line))
+                           (setq evil-insert-state-tag (kevin/faicon-icon-with-text "chevron-right" "IN" 'success))
+                           (setq evil-motion-state-tag (kevin/faicon-icon-with-text "chevron-right" "MO" 'warning))
+                           (setq evil-visual-state-tag (kevin/faicon-icon-with-text "chevron-right" "VI" 'error))
+                           (setq evil-operator-state-tag (kevin/faicon-icon-with-text "chevron-right" "OP" 'font-lock-doc-face))
+                           evil-mode-line-tag
+                           ))
+
+(defun modeline-init ()
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (modeline-active))
+                          (face0 (if active 'mode-line 'mode-line-inactive))
+                          (lhs (list
+                                (evil-tag-segment)
+                                (window-number-segment 'face0)
+                                (buffer-info-segment 'face0)
+                                (minor-mode-segment 'face0)
+                                (position-info-segment 'face0)
+                                ))
+                          (rhs (list
+                                (vsc-info-segment 'warning)
+                                (major-mode-segment)
+                                (flycheck-segment)
+                                (buffer-encoding-segment 'face0)
+                                (timestamp-info-segment 'face0)
+                                ))
+                          )
+                     (concat
+                      (modeline-render-segment-list lhs)
+                      (modeline-fill (modeline-width rhs) 'face0)
+                      (modeline-render-segment-list rhs)
+                      )))))
+
+  )
+
+(add-hook 'after-init-hook #'modeline-init)
 
 (provide 'init-modeline)
 ;;; init-modeline ends here
