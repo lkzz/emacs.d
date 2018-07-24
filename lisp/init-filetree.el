c16e3ec66f8d84c85e23f0daa7b0d24a28389f26
liangkai <liangkai@bilibili.com>
2018-07-24 10:21:30 +0800

    remove neotree-quick-look keybinds

--- lisp/init-filetree.el
+++ lisp/init-filetree.el
@@ -45,7 +45,7 @@
     (eval-after-load 'evil
       '(progn
          (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
-         (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+         ;; (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
          (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
          (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
          (evil-define-key 'normal neotree-mode-map (kbd "h") 'neotree-select-up-node)
