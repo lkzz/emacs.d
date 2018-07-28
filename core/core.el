b69b390fb969f716a50cb119077b316905ddcfe9
liangkai <kevin.scnu@gmail.com>
2018-07-28 19:46:00 +0800

    update projectile-ag keybinds

---
+++ core/core.el
@@ -0,0 +1,3 @@
+(load  (expand-file-name "core/core-variables.el" user-emacs-directory))
+(load  (expand-file-name "core/core-functions.el" user-emacs-directory))
+(load  (expand-file-name "core/core-elpa.el" user-emacs-directory))
