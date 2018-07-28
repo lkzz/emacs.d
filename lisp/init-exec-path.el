c43937c2ecc8ba861d288628ae9716e9c1708e31
liangkai <kevin.scnu@gmail.com>
2018-07-28 10:50:39 +0800

    use :if keywords in use-package

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -6,14 +6,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (memq window-system '(mac ns x))
-  (use-package exec-path-from-shell
-    :defer t
-    :init
-    (setq exec-path-from-shell-check-startup-files nil)
-    (setq exec-path-from-shell-variables '("PATH" "MANPATH" "PYTHONPATH" "GOPATH" "GOROOT" "GO15VENDOREXPERIMENT"))
-    (setq exec-path-from-shell-arguments '("-l"))
-    (add-hook 'after-init-hook 'exec-path-from-shell-initialize)))
+(use-package exec-path-from-shell
+  :defer t
+  :if (memq window-system '(mac ns))
+  :init
+  (setq exec-path-from-shell-check-startup-files nil)
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "PYTHONPATH" "GOPATH" "GOROOT"))
+  (setq exec-path-from-shell-arguments '("-l"))
+  (add-hook 'after-init-hook 'exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
 ;;; init-exec-path ends here
