d9b0afd7630e2fb338aa692b1b8ecff3356b92c0
liangkai <kevin.scnu@gmail.com>
2018-09-06 00:01:15 +0800

    comment helm

--- init.el
+++ init.el
@@ -61,8 +61,8 @@
 
 ;; misc packages
 (require 'init-anzu)
-;; (require 'init-ivy)
-(require 'init-helm)
+(require 'init-ivy)
+;; (require 'init-helm)
 (require 'init-chinese)
 (require 'init-misc)
 ;; (require 'init-email)
