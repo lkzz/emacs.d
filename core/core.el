8e367d724d0b6f4a87537715a3e6697ad0a33ea0
liangkai <kevin.scnu@gmail.com>
2019-01-05 20:56:52 +0800

    update license

--- core/core.el
+++ core/core.el
@@ -1,9 +1,16 @@
-;;; core.el -- files must be load in my emacs. -*- lexical-binding: t -*-
+;;; core.el -- files must be load in my emacs. -*- lexical-binding: t; -*-
 ;;
-;; Author: kevin <kevin.scnu@gmail.com>
+;; Copyright (C) 2017-2019  Kevin Leung
+;;
+;; Author: Kevin Leung <kevin.scnu@gmail.com>
 ;; URL: https://github.com/lkzz/emacs.d
 ;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
 ;;; Commentary:
+;;
 ;;; Code:
 
 (require 'core-elpa)
