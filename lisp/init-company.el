99b199fefb8da191bfa3f66cd4a7d4d951c2a059
liangkai <kevin.scnu@gmail.com>
2019-01-12 16:31:19 +0800

    add company-box

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -84,6 +84,7 @@
   :after company
   :diminish company-box-mode
   :hook (company-mode . company-box-mode)
+  :load-path "vendor/lisp/company-box-20180607.1545"
   :init
   (setq company-box-enable-icon t)
   (setq company-box-doc-delay 0.5)
