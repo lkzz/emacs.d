f767c3ada0797b214ca22e87b3339d93e781349d
Kevin Leung <kevin.scnu@gmail.com>
2019-02-28 10:34:43 +0800

    update awesome-tab keybinds

--- lisp/init-awesome-tab.el
+++ lisp/init-awesome-tab.el
@@ -21,10 +21,10 @@
     (define-key evil-normal-state-map (kbd ",tt") 'awesome-tab-switch-group)
     (define-key evil-normal-state-map (kbd ",ta") 'awesome-tab-select-beg-tab)
     (define-key evil-normal-state-map (kbd ",te") 'awesome-tab-select-end-tab)
-    (define-key evil-normal-state-map (kbd ",th") 'awesome-tab-move-current-tab-to-left)
-    (define-key evil-normal-state-map (kbd ",tl") 'awesome-tab-move-current-tab-to-right)
-    (define-key evil-normal-state-map (kbd ",tn") 'awesome-tab-forward)
-    (define-key evil-normal-state-map (kbd ",tp") 'awesome-tab-backward))
+    (define-key evil-normal-state-map (kbd ",t<") 'awesome-tab-move-current-tab-to-left)
+    (define-key evil-normal-state-map (kbd ",t>") 'awesome-tab-move-current-tab-to-right)
+    (define-key evil-normal-state-map (kbd ",th") 'awesome-tab-forward)
+    (define-key evil-normal-state-map (kbd ",tl") 'awesome-tab-backward))
   (setq awesome-tab-cycle-scope 'tabs) ; Navigate through visible tabs only.
   (awesome-tab-mode t))
 
