2e8077db5817e864c105cd0f1e9b63d381c7f056
Kevin Leung <kevin.scnu@gmail.com>
2019-03-07 20:34:53 +0800

    fix chinese font

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -13,29 +13,24 @@
 ;;
 ;;; Code:
 
-(defvar kevin-english-font "Monaco"
+(defvar kevin-english-font "DejaVu Sans Mono"
   "The font name of English.")
 
-(defvar kevin-chinese-font "Songti"
+(defvar kevin-chinese-font "STKaiti"
   "The font name for Chinese.")
 
-(defvar kevin-fontsize-pair '(15 . 18)
+(defvar kevin-fontsize-pair '(14 . 16.5)
   "Default font size pair for (english . chinese)")
 
-(defvar kevin-fontsize-pair-list '((5 .  6)
-                                   (10 . 12)
-                                   (13 . 16)
-                                   (15 . 18)
-                                   (17 . 20)
-                                   (19 . 22)
-                                   (20 . 24)
-                                   (21 . 26)
-                                   (24 . 28)
-                                   (26 . 32)
-                                   (28 . 34)
-                                   (30 . 36)
-                                   (34 . 40)
-                                   (36 . 44))
+(defvar kevin-fontsize-pair-list '((9    . 10.5)
+                                   (10   . 12.0)
+                                   (11.5 . 14.0)
+                                   (12.5 . 14.5)
+                                   (14   . 16.5)
+                                   (15   . 18.0)
+                                   (16   . 20.0)
+                                   (18   . 22.5))
+
   "This list is used to store matching (englis . chinese) font-size.")
 
 (defun kevin-font-exist-p (fontname)
@@ -44,15 +39,27 @@
       nil
     (if (not (x-list-fonts fontname)) nil t)))
 
-(defun kevin/set-font (english chinese size-pair)
-  "Setup emacs English and Chinese font on x window-system."
+(defun kevin/set-pair-font (english chinese size-pair)
+  "Setup emacs English and Chinese font on mac."
   (if (kevin-font-exist-p english)
-      (set-frame-font (format "%s:pixelsize=%d" english (car size-pair)) t))
+      (let* ((fontname (car (x-list-fonts english nil nil 1))))
+        (if fontname
+            (set-face-attribute 'default nil
+                                :font (font-spec :name fontname
+                                                 :weight 'normal
+                                                 :slant 'normal
+                                                 :size (cdr size-pair)))
+          (message (format "english font:%s not exist" english))))
+    (message (format "english font:%s not exist" english)))
 
   (if (kevin-font-exist-p chinese)
       (dolist (charset '(kana han symbol cjk-misc bopomofo))
-        (set-frame-font (frame-parameter nil 'font) charset
-                        (font-spec :family chinese :size (cdr size-pair))))))
+        (set-fontset-font (frame-parameter nil 'font)
+                          charset (font-spec :family chinese
+                                             :size (cdr size-pair)
+                                             :weight 'normal
+                                             :slant 'normal)))
+    (message (format "chinese font:%s not exist" chinese))))
 
 (defun kevin/set-step-fontsize (step)
   "Increase/Decrease emacs's font size."
@@ -61,19 +68,23 @@
     (setq kevin-fontsize-pair
           (or (cadr (member kevin-fontsize-pair scale-steps))
               kevin-fontsize-pair))
-    (when kevin-fontsize-pair
-      (message "emacs font size set to %.1f" (car kevin-fontsize-pair))
-      (kevin/set-font kevin-english-font kevin-chinese-font kevin-fontsize-pair))))
+    (if kevin-fontsize-pair
+        (progn
+          (message "Font(%s,%s) fontsize set to %.1f" kevin-english-font kevin-chinese-font (car kevin-fontsize-pair))
+          (kevin/set-pair-font kevin-english-font kevin-chinese-font kevin-fontsize-pair))
+      (message "Font(%s,%s) fontsize is already the max/min pair" kevin-english-font kevin-chinese-font))))
 
 (defun kevin/increase-fontsize ()
   "Decrease emacs's font-size acording kevin-fontsize-pair-list."
-  (interactive) (kevin/set-step-fontsize 1))
+  (interactive)
+  (kevin/set-step-fontsize 1))
 
 (defun kevin/decrease-fontsize ()
   "Increase emacs's font-size acording kevin-fontsize-pair-list."
-  (interactive) (kevin/set-step-fontsize -1))
+  (interactive)
+  (kevin/set-step-fontsize -1))
 
 ;; Setup font size based on font-size-pair
-(kevin/set-font kevin-english-font kevin-chinese-font kevin-fontsize-pair)
+(kevin/set-pair-font kevin-english-font kevin-chinese-font kevin-fontsize-pair)
 
 (provide 'init-font)
