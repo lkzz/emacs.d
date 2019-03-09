132c1f9a2b05bd3ffa77575b73f7d00606883b27
Kevin Leung <kevin.scnu@gmail.com>
2019-03-09 15:55:35 +0800

    format code

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -15,14 +15,14 @@
 
 (use-package counsel
   :defer t
+  :ensure t
   :diminish ivy-mode counsel-mode
   :init
-  (kevin/set-leader-keys
-   "SPC" 'counsel-M-x
-   "/" 'counsel-rg
-   "ff" 'counsel-find-file
-   "fr" 'counsel-recentf
-   "ss" 'swiper)
+  (kevin/set-leader-keys "SPC" 'counsel-M-x
+                         "/" 'counsel-rg
+                         "ff" 'counsel-find-file
+                         "fr" 'counsel-recentf
+                         "ss" 'swiper)
   :bind (("C-s" . swiper)
          ("C-S-s" . swiper-all)
          ("C-c C-r" . ivy-resume)
@@ -55,15 +55,17 @@
          ([escape] . minibuffer-keyboard-quit)
          :map counsel-find-file-map
          ("C-h" . counsel-up-directory)
+         ("C-l" . counsel-down-directory)
          :map swiper-map
          ("M-%" . swiper-query-replace))
   :hook ((after-init . ivy-mode)
          (after-init . counsel-mode))
   :config
-  (setq enable-recursive-minibuffers t) ; Allow commands in minibuffers
-  (setq ivy-use-selectable-prompt t)
-  (setq ivy-use-virtual-buffers t)    ; Enable bookmarks and recentf
   (setq ivy-height 12)
+  (setq ivy-use-selectable-prompt t)
+  (setq enable-recursive-minibuffers t) ; Allow commands in minibuffers
+  ;;add recent files and bookmarks to ‘ivy-switch-buffer’
+  (setq ivy-use-virtual-buffers t)
   (setq ivy-fixed-height-minibuffer t)
   (setq ivy-format-function #'ivy-format-function-arrow)
   (setq ivy-count-format "(%d/%d) ")
@@ -75,16 +77,13 @@
   (setq swiper-action-recenter t)
   (setq counsel-find-file-at-point t)
   (setq counsel-yank-pop-separator "\n-------\n")
-  ;; Integration with `projectile'
-  (with-eval-after-load 'projectile
-    (setq projectile-completion-system 'ivy))
-
   ;; Integration with `magit'
   (with-eval-after-load 'magit
     (setq magit-completing-read-function 'ivy-completing-read)))
 
 ;; Ivy integration for Projectile
 (use-package counsel-projectile
+  :ensure t
   :after (counsel projectile)
   :init (counsel-projectile-mode 1))
 
@@ -97,44 +96,41 @@
   (setq ivy-rich-path-style 'abbrev)
   (setq ivy-virtual-abbreviate 'full)
   (setq ivy-rich--display-transformers-list
-        '(ivy-switch-buffer
-          (:columns
-           ((ivy-rich-candidate (:width 30))  ; return the candidate itself
-            (ivy-rich-switch-buffer-size (:width 10 :face warning))  ; return the buffer size
-            (ivy-rich-switch-buffer-major-mode (:width 20 :face error))          ; return the major mode info
-            (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
-            (ivy-rich-switch-buffer-path (:width (lambda (x)
-                                                   (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3)))
-                                                 :face font-lock-comment-face)))
-           :predicate (lambda (cand) (get-buffer cand)))
-          counsel-M-x
-          (:columns
-           ((counsel-M-x-transformer (:width 40))  ; thr original transfomer
-            (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the command
-          counsel-describe-function
-          (:columns
-           ((counsel-describe-function-transformer (:width 40))  ; the original transformer
-            (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the function
-          counsel-describe-variable
-          (:columns
-           ((counsel-describe-variable-transformer (:width 40))  ; the original transformer
-            (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))  ; return the docstring of the variable
-          counsel-recentf
-          (:columns
-           ((ivy-rich-candidate (:width 0.8)) ; return the candidate itself
-            (ivy-rich-file-last-modified-time (:face font-lock-comment-face)))) ; return the last modified time of the file)
-          counsel-bookmark
-          (:columns
-           ((ivy-rich-candidate (:width 20 :face success)) ; return the candidate itself
-            (ivy-rich-bookmark-info (:face font-lock-comment-face)))) ; return the last modified time of the file)
-          )
-        )
+        '(ivy-switch-buffer (:columns ((ivy-rich-candidate (:width 30))  ; return the candidate itself
+                                       (ivy-rich-switch-buffer-size (:width 10 :face warning))  ; return the buffer size
+                                       (ivy-rich-switch-buffer-major-mode (:width 20 :face error))          ; return the major mode info
+                                       (ivy-rich-switch-buffer-project (:width 15 :face success))             ; return project name using `projectile'
+                                       (ivy-rich-switch-buffer-path (:width (lambda (x)
+                                                                              (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3)))
+                                                                            :face font-lock-comment-face)))
+                                      :predicate (lambda (cand) (get-buffer cand)))
+                            counsel-M-x  (:columns ((counsel-M-x-transformer (:width 40))  ; thr original transfomer
+                                                    (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the command
+                            counsel-describe-function (:columns ((counsel-describe-function-transformer (:width 40))  ; the original transformer
+                                                                 (ivy-rich-counsel-function-docstring (:face font-lock-doc-face))))  ; return the docstring of the function
+                            counsel-describe-variable (:columns((counsel-describe-variable-transformer (:width 40))  ; the original transformer
+                                                                (ivy-rich-counsel-variable-docstring (:face font-lock-doc-face))))  ; return the docstring of the variable
+                            counsel-recentf (:columns ((ivy-rich-candidate (:width 0.8)) ; return the candidate itself
+                                                       (ivy-rich-file-last-modified-time (:face font-lock-comment-face)))) ; return the last modified time of the file)
+                            counsel-bookmark (:columns ((ivy-rich-candidate (:width 20 :face success)) ; return the candidate itself
+                                                        (ivy-rich-bookmark-info (:face font-lock-comment-face)))) ; return the last modified time of the file)
 
+                            counsel-find-file (:columns ((ivy-rich-candidate (:width 0.5))
+                                                         (ivy-rich-file-last-modified-time (:face font-lock-comment-face))))
+                            )
+        )
   (ivy-rich-mode 1))
+
+(use-package all-the-icons-ivy
+  :ensure t
+  :defer t
+  :config
+  (all-the-icons-ivy-setup))
 
 ;; For better fuzzy searching
 (use-package flx
   :ensure t
+  :defer t
   :init
   (setq ivy-re-builders-alist
         '((counsel-grep . ivy--regex-plus)
