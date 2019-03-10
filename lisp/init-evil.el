28106125afd15999640f580a8ac9f54a96170b4f
Kevin Leung <kevin.scnu@gmail.com>
2019-03-10 20:49:03 +0800

    update evil keybindings

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -24,15 +24,14 @@
 (use-package evil
   :ensure t
   :hook (after-init . evil-mode)
-  :preface
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
   :init
   (setq evil-magic t
         evil-echo-state t
         evil-default-state 'normal
         evil-want-C-u-scroll t
         evil-want-Y-yank-to-eol t
+        evil-want-integration t
+        evil-want-keybinding nil
         evil-want-visual-char-semi-exclusive t
         evil-indent-convert-tabs t
         evil-ex-search-vim-style-regexp t
@@ -45,7 +44,7 @@
         shift-select-mode nil
         evil-cross-lines t
         evil-move-cursor-back t ;; Move back the cursor one position when exiting insert mode
-        evil-esc-delay 0
+        evil-esc-delay 0.01
         evil-mode-line-format 'after)
   ;; ;; evil cursor color
   (setq  evil-default-cursor '("SkyBlue2" box)
@@ -57,48 +56,38 @@
   :config
   ;; evil ex command
   (evil-ex-define-cmd "W" 'evil-write-all)
-
-  ;; evil insert state keybinds
-  (define-key evil-insert-state-map (kbd "C-a") 'mwim-beginning-of-code-or-line)
-  (define-key evil-insert-state-map (kbd "C-e") 'mwim-end-of-code-or-line)
-  (define-key evil-insert-state-map (kbd "C-k") 'kill-line)
-  (define-key evil-insert-state-map (kbd "C-p") 'evil-previous-visual-line)
-  (define-key evil-insert-state-map (kbd "C-n") 'evil-next-visual-line)
-
+  ;; evil normal,visual,motion state keybinds
+  (evil-define-key '(normal visual motion) 'global (kbd "C-a") 'mwim-beginning-of-code-or-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-e") 'mwim-end-of-code-or-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-k") 'kill-whole-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-p") 'evil-previous-visual-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-n") 'evil-next-visual-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "j") 'evil-next-visual-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "k") 'evil-previous-visual-line)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-i") 'evil-jump-forward)
+  (evil-define-key '(normal visual motion) 'global (kbd "C-o") 'evil-jump-backward)
   ;; evil normal state keybinds
-  (define-key evil-normal-state-map "Y" (kbd "y$"))
-  (define-key evil-normal-state-map (kbd ",w") 'evil-write)
-  (define-key evil-normal-state-map (kbd ",q") 'evil-quit)
-  (define-key evil-normal-state-map (kbd ",y") 'kevin/copy-word)
-  (define-key evil-normal-state-map (kbd ",p") 'kevin/cover-word)
-  (define-key evil-normal-state-map (kbd ",d") 'kevin/delete-word)
-  (define-key evil-normal-state-map (kbd "C-e") 'mwim-end-of-code-or-line)
-  (define-key evil-normal-state-map (kbd "C-a") 'mwim-beginning-of-code-or-line)
-  (define-key evil-normal-state-map (kbd "C-w") 'evil-delete-backward-word)
-  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-  (define-key evil-normal-state-map (kbd "C-n") nil)
-  (define-key evil-normal-state-map (kbd "C-p") nil)
-
-  ;; evil motion state keybinds
-  (define-key evil-motion-state-map (kbd "C-i") 'evil-jump-forward)
-  (define-key evil-motion-state-map (kbd "C-o") 'evil-jump-backward)
-  (define-key evil-motion-state-map (kbd "C-e") 'mwim-end-of-code-or-line)
-
-  ;; evil visual state keybinds
-  (define-key evil-visual-state-map (kbd "C-e") 'end-of-line)
-
+  (evil-define-key 'normal 'global "Y" (kbd "y$"))
+  (evil-define-key 'normal 'global (kbd ",w") 'evil-write)
+  (evil-define-key 'normal 'global (kbd ",q") 'evil-quit)
+  (evil-define-key 'normal 'global (kbd ",y") 'kevin/copy-word)
+  (evil-define-key 'normal 'global (kbd ",p") 'kevin/cover-word)
+  (evil-define-key 'normal 'global (kbd ",d") 'kevin/delete-word)
+  (evil-define-key 'normal 'global (kbd "C-w") 'evil-delete-backward-word)
   ;; Use evil as a default jump handler
   (add-to-list 'kevin-default-jump-handlers 'evil-goto-definition))
 
 (use-package evil-surround
+  :ensure t
   :after evil
   :config
   (global-evil-surround-mode t))
 
 (use-package evil-visualstar
+  :ensure t
   :after evil
   :config
+  ;; search selection in visual state(*:forward #:forward).
   (setq evil-visualstar/persistent t)
   (global-evil-visualstar-mode))
 
@@ -115,10 +104,18 @@
   :ensure t
   :after evil
   :diminish evil-escape-mode
+  :commands (evil-escape evil-escape-mode evil-escape-pre-command-hook)
+  :init
+  (setq evil-escape-key-sequence "jk"
+        evil-escape-delay 0.25
+        evil-escape-excluded-states '(normal visual multiedit emacs motion)
+        evil-escape-excluded-major-modes '(neotree-mode treemacs-mode))
+  (add-hook 'pre-command-hook #'evil-escape-pre-command-hook)
+  (evil-define-key* '(insert replace visual operator) 'global "\C-g" #'evil-escape)
   :config
-  (evil-escape-mode)
-  (setq-default evil-escape-key-sequence "jk")
-  (setq-default evil-escape-delay 0.3))
+  ;; no `evil-escape' in minibuffer
+  (add-hook 'evil-escape-inhibit-functions #'minibufferp)
+  (evil-escape-mode))
 
 (use-package evil-mc
   :ensure t
@@ -136,24 +133,27 @@
         (evil-mc-mode 1)
         (message "evil mc mode enabled"))))
   (kevin/set-leader-keys "tm" #'kevin/toggle-evil-mc)
-  (defun kevin/reset-evil-mc-key-map ()
-    (let ((keys '(("ma" . evil-mc-make-all-cursors)
-                  ("mu" . evil-mc-undo-all-cursors)
-                  ("ms" . evil-mc-pause-cursors)
-                  ("mr" . evil-mc-resume-cursors)
-                  ("mf" . evil-mc-make-and-goto-first-cursor)
-                  ("mb" . evil-mc-make-and-goto-last-cursor)
-                  ("mh" . evil-mc-make-cursor-here)
-                  ("mn" . evil-mc-skip-and-goto-next-match)
-                  ("mp" . evil-mc-skip-and-goto-prev-match)
-                  ("C-n" . evil-mc-make-and-goto-next-match)
-                  ("C-p" . evil-mc-make-and-goto-prev-match)
-                  )))
-      (dolist (key-data keys)
-        ;; (evil-define-key 'normal 'evil-mc-key-map (kbd (car key-data)) (cdr key-data))
-        (evil-define-key 'visual 'evil-mc-key-map (kbd (car key-data)) (cdr key-data)))))
-  :config
-  (kevin/reset-evil-mc-key-map))
+  ;; 清空evil-mc所有默认按键绑定
+  (setq evil-mc-key-map (make-sparse-keymap))
+  ;; 重新自定义按键绑定
+  (evil-define-key 'visual evil-mc-key-map (kbd "ma") 'evil-mc-make-all-cursors)
+  (evil-define-key '(normal visual) evil-mc-key-map (kbd "M-n")  'evil-mc-make-and-goto-next-match)
+  (evil-define-key '(normal visual) evil-mc-key-map (kbd "M-p")  'evil-mc-make-and-goto-prev-match)
+  (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-n")  'evil-mc-skip-and-goto-next-match)
+  (evil-define-key '(normal visual) evil-mc-key-map (kbd "C-p")  'evil-mc-skip-and-goto-prev-match)
+  (evil-define-key 'normal evil-mc-key-map (kbd "ms") 'evil-mc-pause-cursors)
+  (evil-define-key 'normal evil-mc-key-map (kbd "mh") 'evil-mc-make-cursor-here)
+  (evil-define-key 'normal evil-mc-key-map (kbd "mu") 'evil-mc-undo-all-cursors)
+  (evil-define-key 'normal evil-mc-key-map (kbd "mr") 'evil-mc-resume-cursors)
+  (evil-define-key 'normal evil-mc-key-map (kbd "ma") 'evil-mc-make-and-goto-first-cursor)
+  (evil-define-key 'normal evil-mc-key-map (kbd "me") 'evil-mc-make-and-goto-last-cursor)
+  ;; 设置在evil-mc之下可以执行的命令
+  (setq evil-mc-custom-known-commands
+        '((paredit-backward-delete . ((:default . evil-mc-execute-default-call-with-count)))
+          (hungry-delete-backward . ((:default . evil-mc-execute-default-call-with-count)))
+          (org-delete-backward-char . ((:default . evil-mc-execute-default-call-with-count)))
+          ))
+  )
 
 (use-package evil-snipe
   :ensure t
@@ -176,10 +176,10 @@
   :after evil
   :commands evil-vimish-fold-mode
   :diminish evil-vimish-fold-mode
+  :hook (prog-mode . evil-vimish-fold-mode)
   :init
   (setq vimish-fold-dir (concat kevin-cache-directory "vimish-fold/")
-        vimish-fold-indication-mode 'right-fringe)
-  (add-hook 'after-init-hook #'evil-vimish-fold-mode t))
+        vimish-fold-indication-mode 'right-fringe))
 
 (use-package evil-collection
   :ensure t
