a05175e753e89707a67319b09cd0d636e609a608
Kevin Leung <kevin.scnu@gmail.com>
2019-03-28 01:55:53 +0800

    use multiple-cursors instead of evil-mc

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -215,5 +215,18 @@
                ("c p" . counsel-pt)
                ("c f" . counsel-fzf))))
 
+;; Multiple cursors
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c"   . mc/edit-lines)
+         ("C->"           . mc/mark-next-like-this)
+         ("C-<"           . mc/mark-previous-like-this)
+         ("C-c C-<"       . mc/mark-all-like-this)
+         ("C-M->"         . mc/skip-to-next-like-this)
+         ("C-M-<"         . mc/skip-to-previous-like-this)
+         ("s-<mouse-1>"   . mc/add-cursor-on-click)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)
+         :map mc/keymap
+         ("C-|" . mc/vertical-align-with-space)))
+
 (provide 'init-misc)
 ;;; init-misc.el ends here
