019575df8926b643cb89cb65c77eeb86e6a2e3dc
Kevin Leung <kevin.scnu@gmail.com>
2019-04-18 18:23:39 +0800

    disable centered-cussor-mode

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -197,6 +197,7 @@
     (add-to-list 'golden-ratio-exclude-buffer-names n)))
 
 (use-package centered-cursor-mode
+  :disabled
   :commands (centered-cursor-mode global-centered-cursor-mode)
   :diminish centered-cursor-mode "⊝"
   :init
