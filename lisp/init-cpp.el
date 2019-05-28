eb0bc001f59b119e2117d6ed9f703423020c70bb
Kevin Leung <kevin.scnu@gmail.com>
2019-05-28 15:04:10 +0800

    use-package

--- lisp/init-cpp.el
+++ lisp/init-cpp.el
@@ -13,26 +13,11 @@
 ;;
 ;;; Code:
 
-(defun kevin/c-mode-common-setup ()
-  (setq c-default-style "k&r")
-  (setq c-basic-offset 4)
-  ;; give me NO newline automatically after electric expressions are entered
-  (setq c-auto-newline nil)
-  ;; syntax-highlight aggressively
-  ;; (setq font-lock-support-mode 'lazy-lock-mode)
-  (setq lazy-lock-defer-contextually t)
-  (setq lazy-lock-defer-time 0)
-
-  ;; make DEL take all previous whitespace with it
-  (c-toggle-hungry-state 1))
-
-(add-hook 'c-mode-common-hook #'kevin/c-mode-common-setup)
-
-(defun kevin/makefile-mode-setup ()
-  (whitespace-toggle-options '(tabs))
-  (setq indent-tabs-mode t ))
-
-(add-hook 'makefile-mode-hook #'kevin/makefile-mode-setup)
+(use-package cc-mode
+  :ensure nil
+  :config
+  (setq c-default-style "k&r"
+        c-basic-offset 4))
 
 (use-package company-c-headers
   :after company
