4795787e3ab3041802fe4c017c4f4d187fa64152
Kevin Leung <kevin.scnu@gmail.com>
2020-09-12 20:15:36 +0800

    update aggressive-indent-mode

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -92,10 +92,27 @@
 
 ;; Minor mode to aggressively keep your code always indented
 (use-package aggressive-indent
-  :diminish aggressive-indent-mode
-  :hook (emacs-lisp-mode . aggressive-indent-mode)
+  :diminish
+  :hook ((after-init . global-aggressive-indent-mode)
+         ;; FIXME: Disable in big files due to the performance issues
+         ;; https://github.com/Malabarba/aggressive-indent-mode/issues/73
+         (find-file . (lambda ()
+                        (if (> (buffer-size) (* 3000 80))
+                            (aggressive-indent-mode -1)))))
   :config
-  (setq-default aggressive-indent-comments-too t))
+  ;; Disable in some modes
+  (dolist (mode '(asm-mode web-mode html-mode css-mode go-mode scala-mode prolog-inferior-mode))
+    (push mode aggressive-indent-excluded-modes))
+
+  ;; Disable in some commands
+  (add-to-list 'aggressive-indent-protected-commands #'delete-trailing-whitespace t)
+
+  ;; Be slightly less aggressive in C/C++/C#/Java/Go/Swift
+  (add-to-list 'aggressive-indent-dont-indent-if
+               '(and (derived-mode-p 'c-mode 'c++-mode 'csharp-mode
+                                   'java-mode 'go-mode 'swift-mode)
+                   (null (string-match "\\([;{}]\\|\\b\\(if\\|for\\|while\\)\\b\\)"
+                                       (thing-at-point 'line))))))
 
 (use-package savehist
   :ensure nil
