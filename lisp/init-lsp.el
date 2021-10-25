940eda8e4ea7d88ce0433a8b0ed382d2258527f8
liangkai <kevin.scnu@gmail.com>
2021-10-25 13:59:32 +0800

    add dap-mode

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -23,8 +23,8 @@
                          [remap xref-find-references] 'lsp-find-references)
   :init
   ;; @see https://emacs-lsp.github.io/lsp-mode/page/performance
-  (setq read-process-output-max (* 1024 1024)) ;; 1MB
-  (setq lsp-keymap-prefix "C-c l"
+  (setq read-process-output-max (* 1024 1024) ;; 1MB
+        lsp-keymap-prefix "C-c l"
         lsp-keep-workspace-alive nil
         lsp-response-timeout 5
 
@@ -93,6 +93,29 @@
 
   ;; `C-g'to close doc
   (advice-add #'keyboard-quit :before #'lsp-ui-doc-hide))
+
+(use-package dap-mode
+  :defines dap-python-executable
+  :functions dap-hydra/nil
+  :diminish
+  :bind (:map lsp-mode-map
+         ("<f5>" . dap-debug)
+         ("M-<f5>" . dap-hydra))
+  :hook ((after-init . dap-auto-configure-mode)
+         (dap-stopped . (lambda (_args) (dap-hydra)))
+         (dap-terminated . (lambda (_args) (dap-hydra/nil)))
+         (python-mode . (lambda () (require 'dap-python)))
+         (ruby-mode . (lambda () (require 'dap-ruby)))
+         (go-mode . (lambda () (require 'dap-go)))
+         (java-mode . (lambda () (require 'dap-java)))
+         ((c-mode c++-mode objc-mode swift-mode) . (lambda () (require 'dap-lldb)))
+         (php-mode . (lambda () (require 'dap-php)))
+         (elixir-mode . (lambda () (require 'dap-elixir)))
+         ((js-mode js2-mode) . (lambda () (require 'dap-chrome)))
+         (powershell-mode . (lambda () (require 'dap-pwsh))))
+  :init
+  (when (executable-find "python3")
+    (setq dap-python-executable "python3")))
 
 ;; Ivy integration
 (use-package lsp-ivy
